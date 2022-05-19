3a7e74395103d56af875a68bb255e38f5666a556
Viviane Brenner <brenner.vivi@gmail.com>
2022-05-19 18:06:16 -0300

    Preparando revisão de código

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import reactDom from 'react-dom';
-
-import App from './components/App';
-
-import './assets/reset.css';
-import './assets/style.css';
-
-reactDom.render(<App/>,document.querySelector('.root'));
